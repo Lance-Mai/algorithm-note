ef15f89f0e6069cc4826c0732eca08d5289e0317
LanceMai <mailiangshi@gmail.com>
2025-12-24 21:17:09 +0800

    feat(stackandqueue): 添加用栈实现队列的功能
    - 实现了 MyQueue 类，使用两个 LinkedList 模拟栈结构
    - 添加 push、pop、peek 和 empty 四个基本队列操作方法
    - 通过 pushStack 和 popStack 实现队列的先进先出特性
    - 维护 size 变量实时跟踪队列大小
    - 在 peek 操作时处理空队列情况返回 -1

---
+++ labuladong/stackandqueue/MyQueue.java
@@ -0,0 +1,45 @@
+package labuladong.stackandqueue;
+
+import java.util.LinkedList;
+
+/**
+ * 用栈实现队列。15分钟解决，分别用两个LinkedList来模拟， pushStack、popStack
+ */
+public class MyQueue {
+    LinkedList<Integer> pushStack, popStack;
+    int size;
+
+    public MyQueue() {
+        pushStack = new LinkedList<>();
+        popStack = new LinkedList<>();
+        size = 0;
+    }
+
+    public void push(int x) {
+        while (!popStack.isEmpty()) {
+            pushStack.push(popStack.pop());
+        }
+        pushStack.push(x);
+        size++;
+    }
+
+    public int pop() {
+        this.peek();
+        size--;
+        return popStack.pop();
+    }
+
+    public int peek() {
+        if (this.empty()) {
+            return -1; // 表示元素不存在
+        }
+        while (!pushStack.isEmpty()) {
+            popStack.push(pushStack.pop());
+        }
+        return popStack.peek() == null ? -1 : popStack.peek();
+    }
+
+    public boolean empty() {
+        return size == 0;
+    }
+}
