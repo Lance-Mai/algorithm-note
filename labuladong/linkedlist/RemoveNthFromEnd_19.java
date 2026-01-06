43a4532ef298ccd24ef04445f5d3992e66badb13
LanceMai <mailiangshi@gmail.com>
2026-01-06 22:33:33 +0800

    feat(linkedlist): 添加删除链表倒数第N个节点功能
    - 实现双指针算法删除链表倒数第N个节点
    - 使用虚拟头节点简化边界处理
    - 添加前置指针操作实现节点删除
    - 包含手动断开节点引用优化垃圾回收
    - 添加测试用例验证功能正确性

---
+++ labuladong/linkedlist/RemoveNthFromEnd_19.java
@@ -0,0 +1,39 @@
+package labuladong.linkedlist;
+
+/**
+ * 删除链表的倒数第N个节点
+ * 1、指针先跑n步，双指针。用时22分钟。注意需要使用到目标指针的前置指针，才能操作链表节点remove
+ */
+public class RemoveNthFromEnd_19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummyHead = new ListNode();
+        dummyHead.next = head;
+        ListNode cur1 = head;
+        ListNode preCur1 = dummyHead; // cur1的前置节点
+        for (int i = 0; i < n; i++) {
+            cur1 = cur1.next;
+        }
+        ListNode cur2 = head;
+        while (cur1 != null) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+            preCur1 = preCur1.next;
+        }
+        preCur1.next = cur2.next;
+        cur2 = null; // 手动断开cur2，垃圾回收
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        int[] ints = {1, 2, 3, 4, 5};
+        ListNode dummyHead = new ListNode();
+        ListNode cur = dummyHead;
+        for (int i : ints) {
+            cur.next = new ListNode(i);
+            cur = cur.next;
+        }
+        ListNode head = dummyHead.next;
+
+        new RemoveNthFromEnd_19().removeNthFromEnd(head, 2);
+    }
+}
